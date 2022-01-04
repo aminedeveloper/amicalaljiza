d4a3019b6dddf4d17f260aa2f7afcafb23d50995
amine yassine <nizarcht18@gmail.com>
2022-01-04 23:12:35 +0100

    das

--- resources/views/v1/web/pages/single/project.blade.php
+++ resources/views/v1/web/pages/single/project.blade.php
@@ -22,17 +22,21 @@
 
                 <div class="grid portfolio-block-grid">
                     <div class="element-item transition column-3" data-category="transition">
-                        @foreach ($tranche->media as $media)
-                            <div class="col-span-4 sm:col-span-12 res:col-span-4">
-                                <div class="item-wrap">
-                                    <a href="#">
-                                        <figure class="portfolio-image">
-                                            <img src="{{ Storage::url($media->path) }}" alt="Porto Image 1">
-                                        </figure>
-                                    </a>
-                                </div>
+                        <div class="container">
+                            <div class="system-block-wrap grid grid-cols-12 gap-20">
+                                @foreach ($tranche->media as $media)
+                                    <div class="block-system col-span-4 sm:col-span-12 res:col-span-4">
+                                        <div class="item-wrap">
+                                            <a href="#">
+                                                <figure class="portfolio-image">
+                                                    <img src="{{ Storage::url($media->path) }}" alt="Porto Image 1">
+                                                </figure>
+                                            </a>
+                                        </div>
+                                    </div>
+                                @endforeach
                             </div>
-                        @endforeach
+                        </div>
                     </div>
                 </div>
             </div>
